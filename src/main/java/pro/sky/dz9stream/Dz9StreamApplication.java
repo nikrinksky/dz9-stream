bf892728db34cd49ece0aba43d5d98a86d27e415
Nik <nik_rink@ukr.net>
2023-09-16 23:13:09 +0300

    Первый комит в master ветку dz9-stream

---
+++ src/main/java/pro/sky/dz9stream/Dz9StreamApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.dz9stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dz9StreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dz9StreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/dz9stream/Dz9StreamApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.dz9stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dz9StreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
